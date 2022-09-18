aad56194c351f9c9a0b9196335cbc75843f01444
zhangandrew37 <zhang.andrew37@gmail.com>
2022-09-18 03:18:56 -0400

    add: slider

---
+++ src/components/SliderData.tsx
@@ -0,0 +1,15 @@
+export const SliderData = [
+    {
+        name: "Blair House | Rez-One ",
+        location: "Waterloo, ON",
+        image: "https://pbs.twimg.com/media/ExwLVkNXEAsiT82.jpg:large",
+        owned: false
+
+    },
+    {
+        name: "ICON 330",
+        location: "Waterloo, ON",
+        image: "https://www.iconstudents.com/img/icon-building-2.jpg",
+        owned: false
+    },
+]
